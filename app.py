448f49d1d01baa9f6af7f7cf6525dfffbcca2f50
mtruhlar <martintruhlar1@gmail.com>
2022-11-13 12:02:05 +0100

    final_upload

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-class Insurance_list(db.Model):
+class Users_data(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(50))
     email = db.Column(db.String(50))
@@ -23,11 +23,11 @@ class Insurance_list(db.Model):
         self.email = email
         self.phone = phone
 
-@app.route('/')
-def evidence():
-    all_data = Insurance_list.query.all()
 
-    return render_template("index.html", users = all_data)
+@app.route('/')
+def home():
+    users = Users_data.query.all()
+    return render_template("index.html", users = users)
 
 @app.route('/add', methods = ['POST'])
 def insert():
@@ -39,18 +39,30 @@ def insert():
         phone = request.form['phone']
 
 
-        my_data = Insurance_list(name, email, phone)
+        my_data = Users_data(name, email, phone)
         db.session.add(my_data)
         db.session.commit()
+        return redirect(url_for('home'))
 
-        return redirect(url_for('evidence'))
 
 @app.route('/delete/<id>/', methods = ['GET', 'POST'])
 def delete(id):
-    my_data = Insurance_list.query.get(id)
+    my_data = Users_data.query.get(id)
     db.session.delete(my_data)
     db.session.commit()
-    return redirect(url_for('evidence'))
+    return redirect(url_for('home'))
+
+
+@app.route('/update', methods = ['GET', 'POST'])
+def update():
+    if request.method == 'POST':
+        my_data = Users_data.query.get(request.form.get('id'))
+        my_data.name = request.form['name']
+        my_data.email = request.form['email']
+        my_data.phone = request.form['phone']
+        db.session.commit()
+        return redirect(url_for('home'))
+
 
 if __name__ == "__main__":
     with app.app_context():
